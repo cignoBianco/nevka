45e266aea9aebfd58ef3cd1d32cd2ae63db28f52
cignoBianco <smiana123@gmail.com>
2023-07-07 13:27:07 +0300

    init

--- assets/script.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('click', () => {
-    const p = document.createElement('p');
-    p.innerText = 'честно';
-    document.querySelector('body').append(p);
-});
---
+++ assets/scripts/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('click', () => {
+    const p = document.createElement('p');
+    p.innerText = 'честно';
+    document.querySelector('body').append(p);
+});
